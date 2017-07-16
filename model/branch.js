a5695996a90d137b2d574a53e444ebb303305926
kh8 <reczek.krzysztof@gmail.com>
2017-07-16 12:53:44 +0200

    Simple growth algorithm implemented

--- model/branch.js
+++ model/branch.js
@@ -1,14 +1,47 @@
 const Point = require('./point');
 
-function Branch(origin) {
+const SUB_BRANCH_MINIMAL_LENGTH = 50;
+const SUB_BRANCH_REF_POSITION = 0.7;
+const SUB_BRANCH_REF_DIR = 0.1;
+
+function Branch(origin, direction) {
     this.origin = origin;
-    this.length = 3;
-    this.direction = 0.5;
+    this.length = 1;
+    this.direction = direction || 0.5;
+    this.subBranches = {};
 
     this.calculateEndingPoint = function() {
         return new Point(
             this.origin.x + -1 * this.length * Math.cos(Math.PI * this.direction),
             this.origin.y + -1 * this.length * Math.sin(Math.PI * this.direction));
+    };
+
+    this.incrementLength = function() {
+        this.length++;
+    };
+
+    this.propagateGrowth = function() {
+        if (this.subBranches.right) {
+            this.subBranches.right.grow();
+        }
+        if (this.subBranches.left) {
+            this.subBranches.left.grow();
+        }
+    };
+
+    this.createSubBranches = function() {
+        if (this.length === SUB_BRANCH_MINIMAL_LENGTH) {
+            this.subBranches = {
+                right: new Branch(new Point(
+                        this.origin.x + -1 * this.length * SUB_BRANCH_REF_POSITION * Math.cos(Math.PI * (this.direction)),
+                        this.origin.y + -1 * this.length * SUB_BRANCH_REF_POSITION * Math.sin(Math.PI * (this.direction))),
+                    this.direction - SUB_BRANCH_REF_DIR),
+                left: new Branch(new Point(
+                        this.origin.x + -1 * this.length * SUB_BRANCH_REF_POSITION * Math.cos(Math.PI * (this.direction)),
+                        this.origin.y + -1 * this.length * SUB_BRANCH_REF_POSITION * Math.sin(Math.PI * (this.direction))),
+                    this.direction + SUB_BRANCH_REF_DIR),
+            };
+        }
     };
 }
 
@@ -18,6 +51,12 @@ Branch.prototype.getOriginPoint = function() {
 
 Branch.prototype.getEndingPoint = function() {
     return this.calculateEndingPoint();
+};
+
+Branch.prototype.grow = function() {
+    this.incrementLength();
+    this.propagateGrowth();
+    this.createSubBranches();
 };
 
 module.exports = Branch;
--- renderers/canvas.js
+++ renderers/canvas.js
@@ -14,7 +14,20 @@ var origin = new Point(380, 530);
 
 const Branch = require('./model/branch');
 var b1 = new Branch(origin);
-b1.length = 100;
 
 const drawer = require('./renderers/branchDrawer');
-drawer.draw(b1, ctx);
+
+function draw(branch) {
+    drawer.draw(branch, ctx);
+    if (branch.subBranches.right) {
+        draw(branch.subBranches.right);
+    }
+    if (branch.subBranches.left) {
+        draw(branch.subBranches.left);
+    }
+}
+
+setInterval(function() {
+    b1.grow();
+    draw(b1);
+}, 50);
